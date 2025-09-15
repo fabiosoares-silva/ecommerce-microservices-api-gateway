7dab550e679450927641548e177908ce661f515f
fabiosoares-silva <fabiosores.soares@gmail.com>
2025-09-15 18:48:46 -0300

    Criar estrutura base para o projeto de atenticação com jwt

---
+++ AuthService/Controllers/AuthController.cs
@@ -0,0 +1,79 @@
+﻿using AuthService.Data;
+using AuthService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] User user)
+        {
+            if (user == null || string.IsNullOrEmpty(user.Username) || string.IsNullOrEmpty(user.Password))
+            {
+                return BadRequest("Dados de usuário inválidos.");
+            }
+            var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Username == user.Username);
+            if (existingUser != null)
+            {
+                return Conflict("Nome de usuário já existe.");
+            }
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok("Usuário registrado com sucesso.");
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            if (loginModel == null || string.IsNullOrEmpty(loginModel.Username) || string.IsNullOrEmpty(loginModel.Password))
+            {
+                return BadRequest("Dados de login inválidos.");
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == loginModel.Username && u.Password == loginModel.Password);
+
+            if (user == null)
+            {
+                return Unauthorized("Nome de usuário ou senha inválidos.");
+            }
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(""); //TODO Usar uma chave secreta forte e segura. Armazar em um lugar seguro (appsettings.json)
+
+            var tokenDescriptor = new Microsoft.IdentityModel.Tokens.SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+
+                    new Claim(ClaimTypes.Name, user.Username!),
+                    new Claim(ClaimTypes.Role, user.Role ?? "User")
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return Ok(new { user.Id, user.Username, user.Role });
+        }
+
+    }
+}
---
+++ AuthService/Models/LoginModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "O nome do usuário é obrigatório.")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        public string? Password { get; set; }
+    }
+}
